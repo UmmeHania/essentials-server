95d6c6d6c4f07c5683ae62434e8edab64ef51f09
UmmeHania <87921374+UmmeHania@users.noreply.github.com>
2022-05-15 03:24:39 +0600

    fixed quantity object error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,48 +9,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ccuzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
     try {
         await client.connect();
-        const productCollection = client.db('essentials').collection('product');
-        const addedProductCollection = client.db('essentials').collection('addedProduct');
+        const productCollection = client.db("essentials").collection("product");
+        const addedProductCollection = client
+            .db("essentials")
+            .collection("addedProduct");
 
         //Load all data
-        app.get('/inventory', async (req, res) => {
+        app.get("/inventory", async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
-            res.send(products)
-        })
-
+            res.send(products);
+        });
 
         //Add new data
-        app.post('/inventory', async (req, res) => {
+        app.post("/inventory", async (req, res) => {
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
-        })
-
+        });
 
         //Load Single Product details
-        app.get('/inventory/:id', async (req, res) => {
+        app.get("/inventory/:id", async (req, res) => {
             const id = req.params.id;
             // var hex = /[0-9A-Fa-f]{6}/g;
             // const query = (hex.test(id)) ? ObjectId(id) : id;
-            const query = { _id: ObjectId(id) }
-            console.log(id, query)
+            const query = { _id: ObjectId(id) };
+            //   console.log(id, query);
             const product = await productCollection.findOne(query);
             res.send(product);
-        })
+        });
 
         //DELETE single product
-        app.delete('/inventory/:id', async (req, res) => {
+        app.delete("/inventory/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await productCollection.deleteOne(query);
@@ -66,35 +67,35 @@ async function run() {
         });
 
         //Update delivered product
-
-        app.put('/inventory/:id', async (req, res) => {
+        //Update delivered product
+        app.put("/inventory/:id", async (req, res) => {
             const id = req.params.id;
             const updatedProduct = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
-                    quantity: updatedProduct
-                }
+                    ...updatedProduct
+
+                },
             };
-            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            const result = await productCollection.updateOne(
+                filter,
+                updatedDoc,
+                options
+            );
             res.send(result);
+            console.log(id, updatedProduct, filter);
         });
-
-
-
-    }
-    finally {
-
+    } finally {
     }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("essentials running!")
+app.get("/", (req, res) => {
+    res.send("essentials running!");
 });
 
 app.listen(port, () => {
-    console.log('essentials is running on port', port)
+    console.log("essentials is running on port", port);
 });
